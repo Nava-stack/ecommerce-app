a0806897421cb2fd7f2be5af2b85935742f83bef
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-10-08 10:10:20 +0530

    Add home route and view

---
+++ resources/views/home/partials/hero.blade.php
@@ -0,0 +1,9 @@
+<div class="hero_area">
+    <!-- header section strats -->
+    @include('home.partials.header')
+    <!-- end header section -->
+
+    <!-- slider section -->
+    @include('home.partials.slider')
+    <!-- end slider section -->
+  </div>
